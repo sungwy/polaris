eff3b18a521aeea76ea3aebfa26a946c3beb3399
Sung Yun (CODE SIGNING KEY) <sungwy@apache.org>
2026-02-17 20:58:43 -0500

    adopt feedback - thanks dmitri

--- polaris-core/src/main/java/org/apache/polaris/core/auth/AuthorizationRequest.java
+++ polaris-core/src/main/java/org/apache/polaris/core/auth/AuthorizationRequest.java
@@ -67,6 +67,11 @@ public interface AuthorizationRequest {
     return getTargetBindings().stream().map(AuthorizationTargetBinding::getTarget).toList();
   }
 
+  /** Returns true if the request targets principal entities. */
+  default boolean hasPrincipalTarget() {
+    return hasSecurableType(PolarisEntityType.PRINCIPAL);
+  }
+
   /**
    * Returns secondary securables, if any.
    *
